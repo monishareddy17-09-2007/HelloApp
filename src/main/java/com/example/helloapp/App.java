c687f482591fc1422e39d6f95a975350dd60d71d
MONISHA11092007 <mp8928@srmist.edu.in>
2026-03-27 11:02:09 +0530

    [monisha} Added uc2

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -1,10 +1,24 @@
 package com.example.helloapp;
-
+import java.util.*;
 /**
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	public class App {
+    	    public static void main(String[] args) {
+    	        
+    	        // Check if user provided a name as command-line argument
+    	        if (args.length > 0) {
+    	            // Get the name from command-line argument
+    	            String name = args[0];
+    	            
+    	            // Display personalized greeting
+    	            System.out.println("Hello, " + name);
+    	        } else {
+    	            // No argument provided, display default
+    	            System.out.println("Hello World");
+    	        }
+    	        
+    	    }
+    
     }
-}
+
