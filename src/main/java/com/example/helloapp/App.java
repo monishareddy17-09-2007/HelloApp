2e2acb99fb6d9fbd2fe4c100dc04586afa233986
MONISHA11092007 <mp8928@srmist.edu.in>
2026-04-07 23:05:03 +0530

    [Monisha] Added:UC5

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -1,20 +1,17 @@
 package com.example.helloapp;
 
-public class App {
-    public static void main(String[] args) {
-        
-        
-        if (args.length > 0) {
-            
-            for (int i = 0; i < args.length; i++) {
-                String name = args[i];
-                System.out.println("Hello " + name);
-            }
-        } else {
-            
-            String defaultName = "World";
-            System.out.println("Hello " + defaultName);
-        }
-        
-    }
-}
+
+    	public class App {
+    	    public static void main(String[] args) {
+
+    	        // If no arguments → default
+    	        if (args.length == 0) {
+    	            System.out.println("Hello World");
+    	        } else {
+    	            // Loop through all names
+    	            for (String name : args) {
+    	                System.out.println("Hello " + name);
+    	            }
+    	        }
+    	    }
+    	}
